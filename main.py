db36344bccb4a33236967b97d272206ec698b2d4
torbinsky <torbinsky@gmail.com>
2016-08-30 12:18:23 -0700

    TW: Added logging and load config from json.

--- main.py
+++ main.py
@@ -1,56 +1,70 @@
 import time
-
 from porc import Client
 from datetime import datetime
 import RPi.GPIO as io
+import json
+import logging
 
-if len(sys.argv) == 6:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
-    api_key = sys.argv[3]
-    threshold = sys.argv[4]
-    power
-else:
-    print('usage: sudo ./main.py SENSOR GPIO_PIN_NUM ORCHESTRATE_KEY TEMP_THRESHOLD OUTPUT_COLLECTION')
-    sys.exit(1)
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Initialize power control port
-power_pin = 23
+# Load application config file
+logger.info('Loading configuration...')
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
+# Initialize state from config settings
+logger.debug('Initializing state...')
+sensor_types = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302
+}
+sensor = sensor_types[config['sensor.type']]
+pin = config['sensor.pinNum']
+api_key = config['orchestrate.apiKey']
+threshold = config['tempShutoffThreshold']
+power_pin = config['heat.powerControlPinNum']
+logger.info('Configuration loaded')
+
+# Initialize power control port for our heating unit
+logger.info('Initializing power...')
 io.setmode(io.BCM)
 io.setup(power_pin, io.OUT)
-
+io.output(power_pin, False) # default turn off
+logger.info('Power initialized')
 
 # create a client using the default AWS US East host: https://api.orchestrate.io
+logger.info('Initializing database connection...')
 client = Client(api_key)
-
 # make sure our API key works
+logger.info('Testing database connection...')
 client.ping().raise_for_status()
+logger.info('Database connection complete')
 
 def do_main_program():
+    logger.debug('Running...')
     while True:
+        logger.debug('Reading temp,humidity...')
         humidity, temp = read_sensor()
+        logger.debug('Temp[%s] Humidity[%s]', temp, humidity)
 
-        # Turn the light off if we hit threshold temp, on otherwise
+        # Turn the heat off if we hit threshold temp, on otherwise
         if(temp > threshold):
-            light_off()
+            heat_off()
         else:
-            light_on()
+            heat_on()
 
         #log_to_db(humidity, temp)
-        print "READING: "
-        print humidity
-        print temp
-        print "\n"
 
         time.sleep(10)
 
-def light_on():
-    print "LIGHT ON\n"
+def heat_on():
+    loggder.debug('HEAT ON')
     switch(power_pin, True)
 
-def light_off():
-    print "LIGHT OFF\n"
+def heat_off():
+    loggder.debug('HEAT OFF')
     switch(power_pin, False)
 
 def read_sensor():
@@ -63,6 +77,8 @@ def log_to_db(humidity, temp):
       "temp": temp,
       "timestamp": datetime.now()
     })
+    # TODO: Log result of the db write
 
 def switch(pin, mode):
+    logger.debug('Setting pin %s to %s', pin, mode)
     io.output(pin, mode)
