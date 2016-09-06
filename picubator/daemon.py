d411d915b855303874e754fa5247fed8554ce865
torbinsky <torbinsky@gmail.com>
2016-09-06 14:03:19 -0700

    TW: Provide an alternative Daemon mode so we can run kitchen verify on a non-arm VM.

--- picubator/daemon.py
+++ picubator/daemon.py
@@ -1,10 +1,16 @@
 import daemon
-
-from core import main
+import time
 
 class PicubatorDaemon(Daemon):
     def run(self):
-        main()
+        if os.environ.get('PICUBATOR_KITCHEN_VERIFY', 'OFF') == 'ON':
+            while True:
+                print 'WARNING: Kitchen verify mode enabled!'
+                time.sleep(5)
+        else:
+            # Typically want to put imports at top, but that causes issues in non-ARM environments
+            from core import main
+            main()
 
 if __name__ == "__main__":
     daemon = PicubatorDaemon('/tmp/picubator.pid')
