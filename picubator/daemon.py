834a552ad8220377fe455e5a2179d3d6e16ea5a2
torbinsky <torbinsky@gmail.com>
2016-09-06 14:33:08 -0700

    TW: Allow no-op daemon mode for some testing purposes.

--- picubator/daemon.py
+++ picubator/daemon.py
@@ -1,19 +1,26 @@
-import daemon
 import time
+import os
+from daemon import Daemon
+import sys
+import tempfile
+
+def run_daemon():
+    if os.environ.get('PICUBATOR_NOOP_DAEMON', 'OFF') == 'ON':
+        while True:
+            print 'WARNING: no-op daemon enabled!!!'
+            time.sleep(5)
+    else:
+        # Typically want to put imports at top, but that causes issues in non-ARM environments
+        from core import main
+        main()
 
 class PicubatorDaemon(Daemon):
     def run(self):
-        if os.environ.get('PICUBATOR_KITCHEN_VERIFY', 'OFF') == 'ON':
-            while True:
-                print 'WARNING: Kitchen verify mode enabled!'
-                time.sleep(5)
-        else:
-            # Typically want to put imports at top, but that causes issues in non-ARM environments
-            from core import main
-            main()
+        run_daemon()
 
 if __name__ == "__main__":
-    daemon = PicubatorDaemon('/tmp/picubator.pid')
+    temp_dir = tempfile.gettempdir()
+    daemon = PicubatorDaemon(os.path.join(temp_dir, '/picubator.pid'))
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
             daemon.start()
